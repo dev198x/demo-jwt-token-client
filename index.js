6a9f3c16f72e5ec4cef099a741997b80d10f9a7b
Dat Dao <dat.dao@c2c-techhub.io>
2023-10-11 15:04:56 +0700

    update

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const DOMAIN_API = '127.0.0.1'
+
 window.addEventListener('load', () => {
     console.log('start client>>>');
     getToken()
@@ -5,7 +7,7 @@ window.addEventListener('load', () => {
 })
 
 function getToken(){
-    const url = 'http://localhost:3000/auth/login';
+    const url = `http://${DOMAIN_API}:3000/auth/login`;
     const options = {
         method: 'POST',
         body: JSON.stringify({email: 'anonystick@gmail.com', password: 'password'}),
@@ -22,7 +24,7 @@ function getToken(){
 }
 
 function getUsers(){
-    const url = 'http://localhost:3000/api/users';
+    const url = `http://${DOMAIN_API}:3000/api/users`;
     const options = {
         credentials: 'include'
     }
